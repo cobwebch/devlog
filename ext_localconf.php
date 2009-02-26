d2ff1dc469631c86c9a6eddba3b92488f6865a60
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-26 19:24:09 +0000

    Added comments and TODO notes about initialization
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/devlog/trunk@17279 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,11 +1,18 @@
 <?php
-if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+if (!defined ('TYPO3_MODE')) {
+	die ('Access denied.');
+}
 
 // Define the timestamp for the current run
+// TODO: move to tx_devlog constructor (as static variables)
 
 if (!$TYPO3_CONF_VARS['EXTCONF'][$_EXTKEY]['mstamp']) {
-	$parts = explode(' ',microtime());
-	$TYPO3_CONF_VARS['EXTCONF'][$_EXTKEY]['mstamp'] = (string)$parts[1].(string)intval((float)$parts[0]*10000.0);
+	$parts = explode(' ', microtime());
+		// Timestamp with microseconds to make sure 2 log runs can always be distinguished
+		// even when happening very close to one another
+		// TODO: improve with microtime(true), but requires PHP > 5
+	$TYPO3_CONF_VARS['EXTCONF'][$_EXTKEY]['mstamp'] = (string)$parts[1] . (string)intval((float)$parts[0] * 10000.0);
+		// Normal timestamp
 	$TYPO3_CONF_VARS['EXTCONF'][$_EXTKEY]['tstamp'] = $parts[1];
 }
 
