9fede2ffa35a9adb0042d442f38f6f7d42e156c8
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-05-28 13:20:24 +0000

    Changed compatibility range to TYPO3 4.5-6.1, references #48605
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/devlog/branches/devlog-2_9@75581 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '2.9.4',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '3.5.0-4.7.99',
+			'typo3' => '4.5.0-6.1.99',
 			'cms' => '',
 		),
 		'conflicts' => array(
