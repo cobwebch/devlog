0282959c3f4441c1153d32a1e733ca314e8c35a0
Francois Suter <francois@typo3.org>
2017-05-29 10:17:51 +0200

    [TASK] Verify compatibility with TYPO3 8 LTS

---
+++ README.md
@@ -0,0 +1,4 @@
+# Developer's Log
+
+TYPO3 extension for logging calls to `GeneralUtility:devLog()` and browsing and searching those entries.
+[Refer to the manual](https://docs.typo3.org/typo3cms/extensions/devlog/) for more details.
--- ext_emconf.php
+++ ext_emconf.php
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '3.0.1',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '7.6.0-8.5.99',
+            'typo3' => '7.6.0-8.9.99',
         ),
         'conflicts' => array(
         ),
